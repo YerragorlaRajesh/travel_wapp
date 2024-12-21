cf73b54fbba15796e52af1f4493c13b67677a8c6
shetty-suhas <shettysuhasc@gmail.com>
2024-12-21 23:03:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-python manage.py shell << EOF
-from travel_wapp.utils import get_rajesh_user
-get_rajesh_user()
-print("Rajesh user setup completed")
-EOF
-
-
-# Start Gunicorn
+python manage.py collectstatic --noinput
 gunicorn travel_wapp.wsgi:application --bind=0.0.0.0:8000
